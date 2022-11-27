9c9332287237a2e7d44cdc40f0cd612d9f22e7b3
Sazal Das <user@users-MacBook-Pro.local>
2022-11-28 00:39:22 +0600

    Add font awesome icon

--- src/main.js
+++ src/main.js
@@ -5,6 +5,27 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 
+/* import the fontawesome core */
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+library.add(fas, far, fab);
+dom.watch();
+
+/* add icons to the library */
+library.add(faUserSecret);
+
+/* add font awesome icon component */
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
