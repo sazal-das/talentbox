d93914e3ce504f271b6e072ac1f9af642e1a7547
Sazal Das <user@users-MacBook-Pro.local>
2022-11-28 22:50:38 +0600

    design login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -40,7 +40,69 @@
         </div>
       </v-col>
       <v-col cols="12" sm="7" md="7">
-        <div class="right-container">User Talentbox es</div>
+        <div class="right-container">
+          <div>
+            <v-tabs v-model="tab" align-with-title>
+              <v-tabs-slider color="purple"></v-tabs-slider>
+
+              <v-tab
+                class="text-capitalize black--text"
+                v-for="item in tabItems"
+                :key="item"
+              >{{ item }}</v-tab>
+            </v-tabs>
+            <v-tabs-items v-model="tab">
+              <v-tab-item v-for="item in items" :key="item">
+                <div class="form-container">
+                  <div class="text-subtitle-1 text-center">Iniciar sesión</div>
+                  <div class="inner-container">
+                    <div class="text-body-1 text-center">Ingresa a Talentbox</div>
+                    <div class="text-center">Es gratis y lo sera siempre</div>
+                    <div class="mt-10">
+                      <v-form ref="form" v-model="valid" lazy-validation>
+                        <v-text-field
+                          hide-details
+                          color="purple"
+                          placeholder="Correo electrónico"
+                          outlined
+                        ></v-text-field>
+                        <v-text-field
+                          class="mt-5"
+                          hide-details
+                          color="purple"
+                          placeholder="Contraseña"
+                          outlined
+                        ></v-text-field>
+                        <div>
+                          <v-btn class="mt-3 text-capitalize" text small>¿Olvido su contraseña?</v-btn>
+                        </div>
+                        <div class="text-center mt-6">
+                          <v-btn
+                            depressed
+                            rounded
+                            color="purple"
+                            class="text-capitalize px-15 py-5 font-weight-bold btn-bg"
+                            dark
+                          >Ingresar</v-btn>
+                        </div>
+                        <div class="text-center mt-5">
+                          <div>
+                            ¿Nuevo en Talentbox?
+                            <a class="purple--text" href>Crear cuenta</a>
+                          </div>
+                        </div>
+                      </v-form>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  class="text-right"
+                  style="position: fixed; bottom: 20px; right: 40px;"
+                >© 2022 Talentbox</div>
+              </v-tab-item>
+            </v-tabs-items>
+          </div>
+        </div>
       </v-col>
     </v-row>
   </div>
@@ -62,6 +124,10 @@ export default {
           src: "/images/image3.png",
         },
       ],
+      tab: null,
+      tabItems: ["Soy un talento", "Soy una empresa", "Publicar oferta"],
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      valid: true,
     };
   },
 };
@@ -96,5 +162,29 @@ export default {
 }
 .header-text {
   padding: 50px 0;
+}
+.form-container {
+  background-color: #faf7fa;
+  padding: 10px;
+  max-width: 500px;
+  border-radius: 17px;
+  margin: 0 auto;
+  margin-top: 150px;
+}
+.btn-bg {
+  background-image: linear-gradient(
+    to right,
+    #7c00c6,
+    #8400c8,
+    #8d00cb,
+    #9500cd,
+    #9d00cf
+  );
+}
+.inner-container {
+  margin-top: 10px;
+  background-color: white;
+  border-radius: 17px;
+  padding: 40px 30px;
 }
 </style>
